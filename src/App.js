2dafc77db4dd540bdcdd6b39ebf524ff69d540a3
Tharindu Dilshan Ekanayake <dilshantharindu8@gmail.com>
2026-04-07 10:08:22 +0530

    setup tailwind and folder structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
+import { Input } from 'postcss';
 import './App.css';
+import InputComponent from './components/InputComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='text-red-500 text-xl font'>hello</h1>
+      <div>
+        <InputComponent 
+          label="Username"
+          name="username"
+          placeholder="Enter your username"
+        />
+      </div>
     </div>
   );
 }
