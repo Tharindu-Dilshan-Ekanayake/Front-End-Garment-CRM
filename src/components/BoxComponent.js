fe424d92d77f2ff2d81d9f63d43cc1fca758f51e
Tharindu Dilshan Ekanayake <dilshantharindu8@gmail.com>
2026-04-11 17:21:10 +0530

    docker

--- src/components/BoxComponent.js
+++ src/components/BoxComponent.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function BoxComponent({ title , children }) {
   return (
     <div
-      className="bg-white px-6 py-6 w-5/6 md:w-[60%] lg:w-1/4 xl:w-1/4 mx-auto rounded-lg"
+      className="w-5/6 md:w-[60%] lg:w-1/4 xl:w-1/4 mx-auto rounded-2xl px-6 py-6 bg-white/10 border border-white/20 shadow-xl backdrop-blur-md"
     >
-      <h2 className="mb-6 text-3xl font-bold text-center text-gray-800 ">
+      <h2 className="mb-6 text-3xl font-bold text-center text-slate-100">
         {title}
       </h2>
       {children}
